aa9de78ef7b801e1655e413894aa3eb512e98574
filipjoe874 <136949814+filipjoe874@users.noreply.github.com>
2026-03-27 16:17:15 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import * as pdfjsLib from "pdfjs-dist";
+
+// 🔥 QUAN TRỌNG NHẤT
+pdfjsLib.GlobalWorkerOptions.workerSrc =
+  "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js";
+
 import { useState } from "react";
 import QRCode from "qrcode";
 
